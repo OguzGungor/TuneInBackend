b664d924f6b96e0e10f203899f58d136524c3b7d
Mustafa Mustafa Güngör <m.oguzgungor@hotmail.com>
2021-03-23 04:10:39 +0300

    structure setup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  res.send("test post response");
+});
+
+/* /test req*/
+router.get('/test', function(req, res, next) {
+  res.send("test response");
+});
+
 module.exports = router;
